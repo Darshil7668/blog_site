fd607854a77cf274e3c42682c828084527a81092
Darshil <122771378+Darshil7668@users.noreply.github.com>
2023-05-19 18:05:58 +0530

    Update PostCard.jsx

--- components/PostCard.jsx
+++ components/PostCard.jsx
@@ -10,11 +10,10 @@ useEffect(() => {
   
   }, []);
     console.log(key)
-    console.log("index")
     console.log(index)
   
   return (
-    <motion.div variants={fadeLeft} initial={fadeLeft.initial} animate={fadeLeft.animate} className='bg-white  shadow-lg rounded-lg  p-0 lg:p-8 pb-12 mb-8 dark:bg-gray-800 dark:bg-opacity-40  '>
+    <motion.div variants={fadeLeft} initial={fadeLeft.initial} animate={fadeLeft.animate} className='bg-white  shadow-lg rounded-lg  p-0 lg:p-8 pb-12 mb-8 dark:bg-gray-800 dark:bg-opacity-40' id={`blog-${index}`>
       <div className='relative overflow-hidden shadow-md pb-80 mb-6 '>
         <div className='dark:bg-black absolute h-80 w-full  z-10 opacity-40'>
         </div>
