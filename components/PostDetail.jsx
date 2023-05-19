932b222e7d130720d37c14d206ab32b38f54c38b
Darshil <122771378+Darshil7668@users.noreply.github.com>
2023-05-19 17:26:33 +0530

    last changes made

--- components/PostDetail.jsx
+++ components/PostDetail.jsx
@@ -31,7 +31,12 @@ const PostDetail = ({ post }) => {
         return <h4 key={index} className="text-md font-semibold mb-4">{modifiedText.map((item, i) => <React.Fragment key={i}>{item}</React.Fragment>)}</h4>;
       case 'image':
         return (
-          
+           <Image
+            alt={obj.title}
+            height={obj.height}
+            width={obj.width}
+            src={obj.src}
+          />
         );
       default:
         return modifiedText;
