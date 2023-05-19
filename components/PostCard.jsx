3b21ccf467fa83e4c6b26c36471fdf4e95ecaa34
Darshil <122771378+Darshil7668@users.noreply.github.com>
2023-05-19 18:03:09 +0530

    Update PostCard.jsx

--- components/PostCard.jsx
+++ components/PostCard.jsx
@@ -5,11 +5,14 @@ import Image from 'next/image';
 import Link from 'next/link'
 import React, {useEffect} from 'react'
 
-const PostCard = ({ post, key }) => {
+const PostCard = ({ post, key , index }) => {
 useEffect(() => {
   
   }, []);
     console.log(key)
+    console.log("index")
+    console.log(index)
+  
   return (
     <motion.div variants={fadeLeft} initial={fadeLeft.initial} animate={fadeLeft.animate} className='bg-white  shadow-lg rounded-lg  p-0 lg:p-8 pb-12 mb-8 dark:bg-gray-800 dark:bg-opacity-40  '>
       <div className='relative overflow-hidden shadow-md pb-80 mb-6 '>
